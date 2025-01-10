8af91fcfce85dc19525febd15974dcbe723cc911
TheSmith0803 <ethan.m.smith0803@gmail.com>
2025-01-09 21:47:33 -0500

    added operators object

---
+++ calculator.js
@@ -0,0 +1,10 @@
+
+
+let operators = {
+    add: function (a, b) {return a + b},
+    subtract: function (a, b) {return a - b},
+    multiply: function (a, b) {return a * b},
+    divide: function (a, b) {return a / b},
+    exponent: function (a, b) {return a ** b},
+    sqrt: function (a) {return a ** (1/2)},
+}
