9ada2a66e77cfb7b2c71a5a56c260aace46cc035
TheSmith0803 <ethan.m.smith0803@gmail.com>
2025-01-16 20:06:21 -0500

    don't know how

---
+++ calculator.js
@@ -0,0 +1,34 @@
+let num1;
+let num2;
+let oper;
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+function operate(n1,n1,op) {
+    switch (oper) {
+        case '+':
+            add(num1, num2);
+        case '-':
+            subtract(num1, num2);
+        case 'x':
+            multiply(num1, num2);
+        case '/':
+            divide(num1, num2);
+    }
+}
+
+//im gonna punch hole in wall
